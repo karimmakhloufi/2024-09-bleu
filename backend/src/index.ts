c5e9043ae5268007f4cefe87dfc05d588a4448f3
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-24 14:30:28 +0200

    GET and PUT hardcoded ads

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,47 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const ads = [
+  {
+    id: 1,
+    title: "Bike to sell",
+    description:
+      "My bike is blue, working fine. I'm selling it because I've got a new one",
+    owner: "bike.seller@gmail.com",
+    price: 100,
+    picture:
+      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+    location: "Paris",
+    createdAt: "2023-09-05T10:13:14.755Z",
+  },
+  {
+    id: 2,
+    title: "Car to sell",
+    description:
+      "My car is blue, working fine. I'm selling it because I've got a new one",
+    owner: "car.seller@gmail.com",
+    price: 10000,
+    picture:
+      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+    location: "Paris",
+    createdAt: "2023-10-05T10:14:15.922Z",
+  },
+];
+
 app.get("/", (_req, res) => {
   res.send("Hello World test reload");
+});
+
+app.get("/ads", (_req, res) => {
+  res.send(ads);
+});
+
+app.post("/ads", (req, res) => {
+  console.log("request body", req.body);
+  ads.push(req.body);
+  res.send("ad has been created");
 });
 
 app.listen(port, () => {
