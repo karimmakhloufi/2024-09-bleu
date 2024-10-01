0c732eb948d56e5cc23fda9671b5d37640dc1ad4
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-10-01 12:28:24 +0200

    Added useEffect to counter page

--- frontend/src/pages/CounterPage.tsx
+++ frontend/src/pages/CounterPage.tsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CounterPage = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(800);
+  const [page, setPage] = useState(0);
+  useEffect(() => {
+    const executeOnMount = () => {
+      console.log("I was executed on mount");
+      setCounter(100);
+    };
+    executeOnMount();
+  }, [page]);
   return (
     <>
       <p>Counter {counter}</p>
+      <p>Page {page}</p>
       <button
         onClick={() => {
           setCounter(counter + 1);
         }}
       >
-        Increase
+        Increase counter
+      </button>
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Increase page
       </button>
     </>
   );
