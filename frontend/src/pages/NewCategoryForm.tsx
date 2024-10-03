c920e7129fa8056f3fba87b463d8db2da73d361e
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-10-03 15:11:40 +0200

    Added react hook form on frontend and used it in new category page

--- frontend/src/pages/NewCategoryForm.tsx
+++ frontend/src/pages/NewCategoryForm.tsx
@@ -1,26 +1,36 @@
 import axios from "axios";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  title: string;
+};
 
 const NewCategoryFormPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    await axios.post("http://localhost:3000/categories", data);
+  };
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        // Read the form data
-        const form = e.target;
-        const formData = new FormData(form as HTMLFormElement);
-
-        // Or you can work with it as a plain object:
-        const formJson = Object.fromEntries(formData.entries());
-        axios.post("http://localhost:3000/categories", formJson);
-      }}
-    >
-      <label>
-        Titre de la catégorie:
-        <br />
-        <input className="text-field" type="text" name="title" />
-      </label>
-      <button className="button">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Titre de la catégorie:
+          <br />
+          <input
+            className="text-field"
+            {...register("title", { required: true })}
+          />
+          {errors.title && (
+            <span className="error-message">This field is required</span>
+          )}
+        </label>
+        <input type="submit" className="button" />
+      </form>
+    </>
   );
 };
 
