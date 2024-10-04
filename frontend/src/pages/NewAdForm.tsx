0909a7ff23e9d9476ebdb6154d530364a417ff7d
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-10-04 10:28:31 +0200

    Used react hook form to create new ad

--- frontend/src/pages/NewAdForm.tsx
+++ frontend/src/pages/NewAdForm.tsx
@@ -1,8 +1,21 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { category } from "../components/Header";
 import { useNavigate } from "react-router-dom";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  title: string;
+  description: string;
+  owner: string;
+  price: number;
+  picture: string;
+  location: string;
+  createdAt: Date;
+  category: number;
+};
 
 const NewAdFormPage = () => {
   const navigate = useNavigate();
@@ -18,77 +31,262 @@ const NewAdFormPage = () => {
     };
     fetchCategories();
   }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ criteriaMode: "all" });
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    await axios.post("http://localhost:3000/ads", data);
+    toast.success("Ad has been added");
+    navigate("/");
+  };
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        try {
-          // Read the form data
-          const form = e.target;
-          const formData = new FormData(form as HTMLFormElement);
-
-          // Or you can work with it as a plain object:
-          const formJson = Object.fromEntries(formData.entries());
-          await axios.post("http://localhost:3000/ads", formJson);
-          toast.success("Ad has been added");
-          navigate("/");
-        } catch (err) {
-          console.log(err);
-          toast.error("An error occured");
-        }
-      }}
-    >
-      <label>
-        Titre de l'annonce:
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <>
+          <label>
+            Titre de l'annonce:
+            <br />
+            <input
+              className="text-field"
+              {...register("title", {
+                minLength: { value: 2, message: "Minimum 2 characters" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="title"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="text" name="title" />
-      </label>
-      <br />
-      <label>
-        Description:
+        <>
+          <label>
+            Description:
+            <br />
+            <input
+              className="text-field"
+              {...register("description", {
+                minLength: { value: 10, message: "Minimum 10 characters" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="description"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="text" name="description" />
-      </label>
-      <br />
-      <label>
-        Vendeur:
+        <>
+          <label>
+            Vendeur:
+            <br />
+            <input
+              className="text-field"
+              {...register("owner", {
+                minLength: { value: 2, message: "Minimum 2 characters" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="owner"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="text" name="owner" />
-      </label>
-      <br />
-      <label>
-        Prix:
+        <>
+          <label>
+            Prix :
+            <br />
+            <input
+              type="number"
+              className="text-field"
+              {...register("price", {
+                min: { value: 0, message: "Minimum 0" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="price"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="number" name="price" />
-      </label>
-      <br />
-      <label>
-        Image:
+        <>
+          <label>
+            Image:
+            <br />
+            <input
+              className="text-field"
+              {...register("picture", {
+                minLength: { value: 2, message: "Minimum 2 characters" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="picture"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="text" name="picture" />
-      </label>
-      <br />
-      <label>
-        Ville:
+        <>
+          <label>
+            Ville :
+            <br />
+            <input
+              className="text-field"
+              {...register("location", {
+                minLength: { value: 2, message: "Minimum 2 characters" },
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="location"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="text" name="location" />
-      </label>
-      <br />
-      <label>
-        Date:
+        <>
+          <label>
+            Date :
+            <br />
+            <input
+              type="date"
+              className="text-field"
+              {...register("createdAt", {
+                required: "This field is required",
+              })}
+            />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="createdAt"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
         <br />
-        <input className="text-field" type="date" name="createdAt" />
-      </label>
-      <br />
-      <select name="category">
-        {categories.map((el) => (
-          <option key={el.id} value={el.id}>
-            {el.title}
-          </option>
-        ))}
-      </select>
-      <button className="button">Submit</button>
-    </form>
+        <>
+          <label>
+            Category :
+            <br />
+            <select {...register("category")}>
+              {categories.map((el) => (
+                <option key={el.id} value={el.id}>
+                  {el.title}
+                </option>
+              ))}
+            </select>
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="createdAt"
+            render={({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => {
+                console.log(message);
+                return (
+                  <Fragment key={type}>
+                    <br />
+                    <span className="error-message">{message}</span>
+                  </Fragment>
+                );
+              })
+            }
+          />
+        </>
+        <input type="submit" className="button" />
+      </form>
+    </>
   );
 };
 
