024823ee10c142dcf402e283dde43883b94a5752
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-11-15 14:23:11 +0100

    Ad details fetched with graphql query

--- frontend/src/pages/AdDetailsPage.tsx
+++ frontend/src/pages/AdDetailsPage.tsx
@@ -1,36 +1,58 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
-import { AdCardProps } from "../components/AdCard";
+
+const GET_AD_BY_ID = gql`
+  query GetAdById($getAdByIdId: Float!) {
+    getAdById(id: $getAdByIdId) {
+      id
+      title
+      description
+      owner
+      price
+      pictures {
+        id
+        url
+      }
+      location
+      createdAt
+      category {
+        id
+        title
+      }
+    }
+  }
+`;
 
 const AdDetailsPage = () => {
   const { id } = useParams();
-  const [adDetails, setAdDetails] = useState<AdCardProps>();
-  useEffect(() => {
-    const fetchAdDetails = async () => {
-      const result = await axios.get(`http://localhost:3000/ads/${id}`);
-      console.log(result);
-      setAdDetails(result.data);
-    };
-    fetchAdDetails();
-  }, [id]);
+  const { data, error, loading } = useQuery(GET_AD_BY_ID, {
+    variables: { getAdByIdId: parseInt(id as string) },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  console.log(data);
   return (
     <div>
-      <h2 className="ad-details-title">{adDetails?.title}</h2>
+      <h2 className="ad-details-title">{data.getAdById.title}</h2>
       <section className="ad-details">
         <div className="ad-details-image-container">
-          <img className="ad-details-image" src={adDetails?.picture} />
+          <img
+            className="ad-details-image"
+            src={data.getAdById.pictures[0].url}
+          />
         </div>
         <div className="ad-details-info">
-          <div className="ad-details-price">{adDetails?.price} €</div>
-          <div className="ad-details-description">{adDetails?.description}</div>
+          <div className="ad-details-price">{data.getAdById.price} €</div>
+          <div className="ad-details-description">
+            {data.getAdById.description}
+          </div>
           <hr className="separator" />
           <div className="ad-details-owner">
-            Annoncée publiée par <b>{adDetails?.owner}</b>{" "}
-            {new Date(adDetails?.createdAt as string).toDateString()}.
+            Annoncée publiée par <b>{data.getAdById.owner}</b>{" "}
+            {new Date(data.getAdById.createdAt as string).toDateString()}.
           </div>
           <a
-            href={`mailto:${adDetails?.owner}`}
+            href={`mailto:${data.getAdById.owner}`}
             className="button button-primary link-button"
           >
             <svg
