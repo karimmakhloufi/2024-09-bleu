63ac394dea7f95f7f4b2ee559a90c6bcfda29795
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-11-19 14:20:13 +0100

    Used codegen query for ad details and ordered pictures

--- backend/src/resolvers/AdResolver.ts
+++ backend/src/resolvers/AdResolver.ts
@@ -10,6 +10,9 @@ class AdResolver {
     const ads = await Ad.find({
       order: {
         id: "DESC",
+        pictures: {
+          id: "DESC",
+        },
       },
     });
     return ads;
@@ -17,7 +20,13 @@ class AdResolver {
 
   @Query(() => Ad)
   async getAdById(@Arg("id") id: number) {
-    const ad = await Ad.findOneByOrFail({ id: id });
+    const ad = await Ad.findOne({
+      where: { id: id },
+      order: { pictures: { id: "DESC" } },
+    });
+    if (ad === null) {
+      throw new Error("Cannot find ad with id " + id);
+    }
     return ad;
   }
 
--- frontend/src/pages/AdDetailsPage.tsx
+++ frontend/src/pages/AdDetailsPage.tsx
@@ -1,79 +1,82 @@
-import { useQuery } from "@apollo/client";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { GET_AD_BY_ID, GET_ALL_ADS } from "../graphql/queries";
-import { useDeleteAdByIdMutation } from "../generated/graphql-types";
+import { GET_ALL_ADS } from "../graphql/queries";
+import {
+  useDeleteAdByIdMutation,
+  useGetAdByIdQuery,
+} from "../generated/graphql-types";
 
 const AdDetailsPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { data, error, loading } = useQuery(GET_AD_BY_ID, {
+  const { data, error, loading } = useGetAdByIdQuery({
     variables: { getAdByIdId: parseInt(id as string) },
   });
   const [deleteAdById] = useDeleteAdByIdMutation();
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-
-  return (
-    <div>
-      <h2 className="ad-details-title">{data.getAdById.title}</h2>
-      <section className="ad-details">
-        <div className="ad-details-image-container">
-          <img
-            className="ad-details-image"
-            src={data.getAdById.pictures[0].url}
-          />
-        </div>
-        <div className="ad-details-info">
-          <div className="ad-details-price">{data.getAdById.price} €</div>
-          <div className="ad-details-description">
-            {data.getAdById.description}
+  if (data) {
+    return (
+      <div>
+        <h2 className="ad-details-title">{data.getAdById.title}</h2>
+        <section className="ad-details">
+          <div className="ad-details-image-container">
+            <img
+              className="ad-details-image"
+              src={data.getAdById.pictures[0].url}
+            />
           </div>
-          <hr className="separator" />
-          <div className="ad-details-owner">
-            Annoncée publiée par <b>{data.getAdById.owner}</b>{" "}
-            {new Date(data.getAdById.createdAt as string).toDateString()}.
-          </div>
-          <a
-            href={`mailto:${data.getAdById.owner}`}
-            className="button button-primary link-button"
-          >
-            <svg
-              aria-hidden="true"
-              width="16"
-              height="16"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-              className="styled__BaseIcon-sc-1jsm4qr-0 llmHhT"
-              stroke="currentcolor"
-              strokeWidth="2.5"
-              fill="none"
+          <div className="ad-details-info">
+            <div className="ad-details-price">{data.getAdById.price} €</div>
+            <div className="ad-details-description">
+              {data.getAdById.description}
+            </div>
+            <hr className="separator" />
+            <div className="ad-details-owner">
+              Annoncée publiée par <b>{data.getAdById.owner}</b>{" "}
+              {new Date(data.getAdById.createdAt as string).toDateString()}.
+            </div>
+            <a
+              href={`mailto:${data.getAdById.owner}`}
+              className="button button-primary link-button"
             >
-              <path d="M25 4H7a5 5 0 0 0-5 5v14a5 5 0 0 0 5 5h18a5 5 0 0 0 5-5V9a5 5 0 0 0-5-5ZM7 6h18a3 3 0 0 1 2.4 1.22s0 0-.08 0L18 15.79a3 3 0 0 1-4.06 0L4.68 7.26H4.6A3 3 0 0 1 7 6Zm18 20H7a3 3 0 0 1-3-3V9.36l8.62 7.9a5 5 0 0 0 6.76 0L28 9.36V23a3 3 0 0 1-3 3Z"></path>
-            </svg>
-            Envoyer un email
-          </a>
-          <Link to={`/ad/edit/${id}`}>
-            <button>Edit</button>
-          </Link>
-          <button
-            onClick={async () => {
-              console.log("delete ad with id", id);
-              if (id) {
-                await deleteAdById({
-                  variables: { deleteAdId: parseInt(id) },
-                  refetchQueries: [GET_ALL_ADS],
-                  awaitRefetchQueries: true,
-                });
-                navigate("/");
-              }
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      </section>
-    </div>
-  );
+              <svg
+                aria-hidden="true"
+                width="16"
+                height="16"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                className="styled__BaseIcon-sc-1jsm4qr-0 llmHhT"
+                stroke="currentcolor"
+                strokeWidth="2.5"
+                fill="none"
+              >
+                <path d="M25 4H7a5 5 0 0 0-5 5v14a5 5 0 0 0 5 5h18a5 5 0 0 0 5-5V9a5 5 0 0 0-5-5ZM7 6h18a3 3 0 0 1 2.4 1.22s0 0-.08 0L18 15.79a3 3 0 0 1-4.06 0L4.68 7.26H4.6A3 3 0 0 1 7 6Zm18 20H7a3 3 0 0 1-3-3V9.36l8.62 7.9a5 5 0 0 0 6.76 0L28 9.36V23a3 3 0 0 1-3 3Z"></path>
+              </svg>
+              Envoyer un email
+            </a>
+            <Link to={`/ad/edit/${id}`}>
+              <button>Edit</button>
+            </Link>
+            <button
+              onClick={async () => {
+                console.log("delete ad with id", id);
+                if (id) {
+                  await deleteAdById({
+                    variables: { deleteAdId: parseInt(id) },
+                    refetchQueries: [GET_ALL_ADS],
+                    awaitRefetchQueries: true,
+                  });
+                  navigate("/");
+                }
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </section>
+      </div>
+    );
+  }
 };
 
 export default AdDetailsPage;
