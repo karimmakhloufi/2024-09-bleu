2199b126788ef988576a379f102c3b2d924513ec
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2025-03-12 12:07:00 +0100

    Added url modularity to end to end basic test

--- e2e/tests/basic.test.ts
+++ e2e/tests/basic.test.ts
@@ -1,7 +1,11 @@
 import { test, expect } from "@playwright/test";
 
+const baseUrl = process.env.LOCAL
+  ? "http://localhost:7000"
+  : "http://api_gateway/";
+
 test("Go to home page", async ({ page }) => {
-  await page.goto("http://api_gateway/");
+  await page.goto(baseUrl);
 
   await page.waitForLoadState("networkidle");
   await expect(page.getByText("Annonces récentes")).toBeVisible();
